61c35ba748cd45bdc5c18e849f8e8218fbb2d1b6
Ruslan Uriadko <deniskalicen@gmail.com>
2024-07-13 00:59:09 +0200

    migrate faker.js to v8

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 const { faker } = require("@faker-js/faker");
+const fakerTypes = require("./fakerTypes.json");
 const properCase = require("proper-case");
-
-// List of all types of Fakers. We specify this explicitly since there is
-// no easy way to filter out these from the other objects on the faker module.
-const fakerTypes = [
-  "address",
-  "animal",
-  "color",
-  "commerce",
-  "company",
-  "database",
-  "datatype",
-  "date",
-  "finance",
-  "git",
-  "hacker",
-  "helpers",
-  "image",
-  "internet",
-  "lorem",
-  "mersenne",
-  "music",
-  "name",
-  "phone",
-  "random",
-  "science",
-  "system",
-  "vehicle",
-  "word",
-];
 
 // Creates the scaffolds for Enum options that Insomnia's Template Tags expect
 populateFakerOptions = function (someArray) {
@@ -67,16 +39,16 @@ populateFakerSubOptions = function () {
   });
 };
 
-populateFakerLocalizationOptions = function () {
-  return Object.keys(faker.locales)
-    .sort()
-    .map(function (locale) {
-      return {
-        displayName: locale,
-        value: locale,
-      };
-    });
-};
+// populateFakerLocalizationOptions = function () {
+//   return Object.keys(faker.locales)
+//     .sort()
+//     .map(function (locale) {
+//       return {
+//         displayName: locale,
+//         value: locale,
+//       };
+//     });
+// };
 
 // Actual Template Tags export that Insomnia expects
 module.exports.templateTags = [
@@ -101,12 +73,12 @@ module.exports.templateTags = [
           description:
             "Allows you to pass in a string that some types allow for more fine grained control over the output of the value. See https://github.com/faker-js/faker for more info.",
         },
-        {
-          displayName: "Localization",
-          type: "enum",
-          options: populateFakerLocalizationOptions(),
-          defaultValue: "en",
-        },
+        // {
+        //   displayName: "Localization",
+        //   type: "enum",
+        //   options: populateFakerLocalizationOptions(),
+        //   defaultValue: "en",
+        // },
       ]),
     async run(_context, type, ...args) {
       let returnValue;
@@ -121,8 +93,8 @@ module.exports.templateTags = [
         subTypeValue = this.args[fakerTypeIndex + 1].options[0].value;
       }
       // Setup faker locale for i18n support
-      var fakerLocale = args.slice(-1)[0];
-      faker.locale = fakerLocale;
+      // var fakerLocale = args.slice(-1)[0];
+      // faker.locale = fakerLocale;
       // Optional Format String
       var optionalModifier = args.slice(-2)[0];
       if (optionalModifier != "") {
